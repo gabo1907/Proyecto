40156f7f47ed2bcd4fc783dc73e82118aa111e39
gabo1907 <eljimmymunoz@gmail.com>
2018-08-03 22:30:01 -0500

    Por favor

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.sidenav').sidenav();
+  //Paralax
+  $('.parallax').parallax();
+   //Carousel
+   $('.carousel').carousel();
+   //Collapside
+     $('.collapsible').collapsible();
+});
